61ba545d94a6341265e5542b984e10a68ef05859
oxtw <weigertmiguel@hotmail.com>
2024-04-15 12:45:21 -0300

    Route Params

--- src/server.js
+++ src/server.js
@@ -2,5 +2,14 @@ const express = require ('express');
 
 const app = express();
 
-const PORT = 3333;
+app.get("/message/:id/:user", (request, response) => {
+
+    const { id, user} = request.params
+
+    response.send(
+        `Id da mensagem: ${id}.
+         Para o usuário: ${user}.`)
+})
+
+const PORT = 3333;// porta que está inicializando a API
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
