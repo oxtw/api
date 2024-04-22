27fdf81cf824ad4b43904591ec6f83557e754fd9
oxtw <weigertmiguel@hotmail.com>
2024-04-22 14:49:36 -0300

    Organizando a estrutura do projeto

--- src/server.js
+++ src/server.js
@@ -1,21 +1,11 @@
 const express = require ('express');
 
+const routes = require("./routes/index.js");
+
 const app = express();
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-
-    const { id, user} = request.params
-
-    response.send(
-        `Id da mensagem: ${id}.
-         Nome do usuário: ${user}.`)
-})
-
-app.get("/users", (request, response) => {
-    const{page, limit} = request.query;
-
-    response.send(`Página: ${page}. Mostrar: ${limit}`);
-});
+app.use(routes);
 
 const PORT = 3333;// porta que está inicializando a API
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
