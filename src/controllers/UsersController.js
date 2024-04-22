398ab2ea6df05a3c929d78ed9c68943292e770fc
oxtw <weigertmiguel@hotmail.com>
2024-04-22 15:14:52 -0300

    Users Controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,18 @@
 class UsersController {
     /**
      * index - GET para listar varios registros.
-     * show-   */
+     * show - GET para exibir um registro específico.
+     * create - POST para poder criar um registro.
+     * update - PUT para atualizar um registro.
+     * delete - DELETE para remover um registro.
+     */
+
+    //método create
+    create(request, response){
+        const {name , email, password} = request.body;
+
+        response.json({name , email, password});
+    }
 }
 
 module.exports= UsersController;
